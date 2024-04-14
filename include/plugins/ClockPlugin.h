815d83aa7f4c43a337f65666a3e8385a8f9a0cd3
Shoulder <40721597+Axelmannen@users.noreply.github.com>
2024-04-14 13:07:27 +0200

    Better handling of brightness state
    Signed-off-by: Shoulder <40721597+Axelmannen@users.noreply.github.com>

--- include/plugins/ClockPlugin.h
+++ include/plugins/ClockPlugin.h
@@ -11,13 +11,15 @@ private:
   int previousMinutes;
   int previousHour;
 
+  // Dim time must encapsulate off time
   int DIM_START {21};
   int OFF_START {22};
   int OFF_END {7};
   int DIM_END {9};
 
-  bool dimMode;
-  bool offMode;
+  int previousState;
+  int state;
+
   short OutlineX[60];
   short OutlineY[60];
 
--- src/plugins/ClockPlugin.cpp
+++ src/plugins/ClockPlugin.cpp
@@ -1,18 +1,17 @@
 #include "plugins/ClockPlugin.h"
 
-ClockPlugin::ClockPlugin() {
-    // List of coordinates
-    int OutlineX[60] = { 8, 9,10,11,12,13,14,
+ClockPlugin::ClockPlugin() : 
+              OutlineX { 8, 9,10,11,12,13,14,
                         15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,
                         14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                         1, 2, 3, 4, 5, 6, 7};
-    int OutlineY[60] = { 0, 0, 0, 0, 0, 0, 0,
-                        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,
+                         1, 2, 3, 4, 5, 6, 7}, 
+              OutlineY { 0, 0, 0, 0, 0, 0, 0,
+                         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,
                         15,15,15,15,15,15,15,15,15,15,15,15,15,15,
-                        15, 14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
-                          0, 0, 0, 0, 0, 0, 0};
-}
+                        15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+                         0, 0, 0, 0, 0, 0, 0} {}
+
 
 void ClockPlugin::setup()
 {
@@ -28,9 +27,11 @@ void ClockPlugin::setup()
   previousMinutes = -1;
   previousHour = -1;
 
-  dimMode = false;
-  offMode = false;
+  previousState = -1;
+  state = 2;
+
 }
+
 
 void ClockPlugin::loop()
 {
@@ -38,48 +39,46 @@ void ClockPlugin::loop()
   {
     if (previousHour != timeinfo.tm_hour || previousMinutes != timeinfo.tm_min)
     {
-      // Toggle dim mode
-      if (!dimMode && (timeinfo.tm_hour >= DIM_START || timeinfo.tm_hour < DIM_END))
-      {
-        dimMode = true;
-        Screen.setBrightness(5);
-      }
-      else if (dimMode && (timeinfo.tm_hour >= DIM_END && timeinfo.tm_hour < DIM_START))
-      {
-        dimMode = false;
-        Screen.setBrightness(255);
-      }
 
-      // Toggle off mode 
-       if (!offMode && (timeinfo.tm_hour >= OFF_START || timeinfo.tm_hour < OFF_END))
+      // Determine state
+      if (timeinfo.tm_hour >= DIM_START || timeinfo.tm_hour < DIM_END)
       {
-        offMode = true;
-        Screen.setBrightness(0);
-      }
-      else if (offMode && (timeinfo.tm_hour >= OFF_END && timeinfo.tm_hour < OFF_START))
-      {
-        offMode = false;
-        Screen.setBrightness(5);
+        if (timeinfo.tm_hour >= OFF_START || timeinfo.tm_hour < OFF_END) {
+          state = 0;
+        } else {
+          state = 1;
+        }
+      } else {
+        state = 2;
       }
 
-
-
+      // Update brightness
+      if (state != previousState) {
+        if (state == 1) {
+          Screen.setBrightness(5);
+        } else if (state == 2) {
+          Screen.setBrightness(255);
+        }
+      }
 
       Screen.clear();
-      Screen.drawNumbers(3, 2, {(timeinfo.tm_hour - timeinfo.tm_hour % 10) / 10, timeinfo.tm_hour % 10});
-      Screen.drawNumbers(3, 9, {(timeinfo.tm_min - timeinfo.tm_min % 10) / 10, timeinfo.tm_min % 10});
+      if (state != 0) {
+        Screen.drawNumbers(3, 2, {(timeinfo.tm_hour - timeinfo.tm_hour % 10) / 10, timeinfo.tm_hour % 10});
+        Screen.drawNumbers(3, 9, {(timeinfo.tm_min - timeinfo.tm_min % 10) / 10, timeinfo.tm_min % 10});
+      }
+      previousState = state;
     }
 
     previousMinutes = timeinfo.tm_min;
     previousHour = timeinfo.tm_hour;
   }
-  if (!dimMode && previousSeconds != timeinfo.tm_sec)
+  if (state == 2 && previousSeconds != timeinfo.tm_sec)
   {
     // Draw outline
-    for (int i = 0; i < timeinfo.tm_sec + 1; i++) // Draw one more pixel to close the rectangle completely
-    {
-      Screen.setPixel(OutlineX[i], OutlineY[i], 1, 5);
-    }
+    // for (int i = 0; i < timeinfo.tm_sec + 1; i++) // Draw one more pixel to close the rectangle completely
+    // {
+    //   Screen.setPixel(OutlineX[i], OutlineY[i], 1, 5);
+    // }
     // Draw seconds
     Screen.setPixel(OutlineX[timeinfo.tm_sec], OutlineY[timeinfo.tm_sec], 1, 5);
     Screen.setPixel(OutlineX[previousSeconds], OutlineY[previousSeconds], 0, 5);
